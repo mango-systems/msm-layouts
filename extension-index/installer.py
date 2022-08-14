af64d46cda325ec9098120b71fcc4449fe5eb485
Spike_Projects <msm74588@gmail.com>
2022-08-14 14:23:29 +0530

    updated index

--- extension-index/installer.py
+++ extension-index/installer.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+## curl https://raw.githubusercontent.com/msm-linux/msm-layouts/master/extension-index/installer.py | python
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+## curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
-print("hello world for world")
+extension_index = {
+    "hello":"world",
+    "hello1":"world1"
+}
+
+print(extension_index)
